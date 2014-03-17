c6dbeb63a4c210b1c3a9cf908c72493d1c458768
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-03-18 00:15:49 +0100

    fix(elasticsearch): corrected index

--- elasticSearchSync.js
+++ elasticSearchSync.js
@@ -2,9 +2,9 @@ var config = require('./config.js');
 var SyncService = require('ninya-sync-elasticsearch');
 
 var syncService = new SyncService({
-    batchSize: 500,
+    batchSize: 350,
     tableName: 'users',
-    elasticsearchIndex: 'production_v2',
+    elasticsearchIndex: 'production_v4',
     dbConnectionString: config.dbConnectionString,
     elasticsearchEndpoint: config.elasticsearchEndpoint
 });
