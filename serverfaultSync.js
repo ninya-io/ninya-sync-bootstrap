c522fd9bedd08609d6e80ac97bd184bc00128d08
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-05-12 00:13:45 +0200

    feat(serverfault): add sync job

---
+++ serverfaultSync.js
@@ -0,0 +1,13 @@
+var config = require('./config.js');
+var StackExchangeSync = require('ninya-sync-stackexchange');
+
+var sync = new StackExchangeSync({
+    index: 'production_v4',
+    elasticsearchEndpoint: config.elasticsearchEndpoint,
+    stackexchangeSite: 'serverfault',
+    maxEntityCount: 160000,
+    pageSize: 10,
+    maxRunTime: 9 * 60 * 1000
+});
+
+sync.safeResume();
